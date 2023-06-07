24feb15dd6ef5804926c9d5b18e9df3db7f67559
Denis Vegi <dvegister@gmail.com>
2023-06-07 11:26:05 -0300

    Corrigindo erros no código

--- src/Cosmeticos/Paginacao/CosmComPage1.tsx
+++ src/Cosmeticos/Paginacao/CosmComPage1.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
